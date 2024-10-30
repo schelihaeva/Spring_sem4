fb85b7a1640676276f24152829c3524d59ae6217
Anastasya <gbshell2022@gmail.com>
2024-10-30 09:00:05 +0300

    sem4

---
+++ java/ru/geekbrains/ProjectHW4/ProjectHw4SpringApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.ProjectHW4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHw4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHw4SpringApplication.class, args);
+    }
+
+}
---
+++ java/ru/geekbrains/ProjectHW4/domain/Input.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.ProjectHW4.domain;
+
+public class Input {
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
